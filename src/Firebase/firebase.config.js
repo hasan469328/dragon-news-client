60ad47f8eb7ef1b2cfa15f128ee30875c06ed849
hasan469328 <hasanur.68.bd@gmail.com>
2023-05-01 00:25:49 +0600

    hide firebaseconfig file

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -3,14 +3,15 @@ import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
+
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyB5HqKM20LPc5B2hBmjmNTdZoYGcgCFdbo",
-  authDomain: "dragon-news-client-57feb.firebaseapp.com",
-  projectId: "dragon-news-client-57feb",
-  storageBucket: "dragon-news-client-57feb.appspot.com",
-  messagingSenderId: "516191387883",
-  appId: "1:516191387883:web:e3a7add89d6fe5a61b3eae"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
